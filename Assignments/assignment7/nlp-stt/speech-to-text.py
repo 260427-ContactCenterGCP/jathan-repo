d6bf72296fce4d5fc16e40a1d016988573bee4ce
jlazala3102 <j.lazala3102@gmail.com>
2026-04-30 14:06:06 -0500

    7b

--- Assignments/assignment7/nlp-stt/speech-to-text.py
+++ Assignments/assignment7/nlp-stt/speech-to-text.py
@@ -1,35 +1,44 @@
-
 from google.cloud.speech_v2 import SpeechClient
 from google.cloud.speech_v2.types import cloud_speech
 
-# YOUR PROJECT ID
 PROJECT_ID = "project-86e5d1df-f58e-4fb6-b2c"
-
-# YOUR LOCATION
 LOCATION = "us-central1"
 
 client = SpeechClient(
     client_options={"api_endpoint": f"{LOCATION}-speech.googleapis.com"}
 )
 
-# Add your audio file here! Test with Sandshrew.wav if you want. Keep your audio short (<30s)
-with open("resources/Sandshrew.wav", "rb") as f:
-    audio_content = f.read()
+audio_files = [
+    "resources/betcatchknife(online-audio-converter.com).mp3",
+    "resources/steponthescale(online-audio-converter.com).mp3"
+]
 
-config = cloud_speech.RecognitionConfig(
-    auto_decoding_config=cloud_speech.AutoDetectDecodingConfig(),
-    language_codes=["en-US"],
-    model="short",
-)
+for audio_file in audio_files:
+    with open(audio_file, "rb") as f:
+        audio_content = f.read()
 
-request = cloud_speech.RecognizeRequest(
-    recognizer=f"projects/{PROJECT_ID}/locations/{LOCATION}/recognizers/_",
-    config=config,
-    content=audio_content,
-)
+    config = cloud_speech.RecognitionConfig(
+        auto_decoding_config=cloud_speech.AutoDetectDecodingConfig(),
+        language_codes=["en-US"],
+        model="short",
+    )
 
-# Transcribes the audio into text
-response = client.recognize(request=request)
+    request = cloud_speech.RecognizeRequest(
+        recognizer=f"projects/{PROJECT_ID}/locations/{LOCATION}/recognizers/_",
+        config=config,
+        content=audio_content,
+    )
 
-for result in response.results:
-    print(f"Transcript: {result.alternatives[0].transcript}")
+    response = client.recognize(request=request)
+
+    print(f"Audio File: {audio_file}")
+
+    if len(response.results) == 0:
+        print("Transcript: No transcript detected")
+        print("Confidence: N/A")
+    else:
+        for result in response.results:
+            print(f"Transcript: {result.alternatives[0].transcript}")
+            print(f"Confidence: {result.alternatives[0].confidence}")
+
+    print()
